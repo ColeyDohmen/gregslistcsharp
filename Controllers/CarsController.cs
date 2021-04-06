7d92e7a151126b1741c5c529cf5517aeeb819387
ColeyDohmen <coley.dawn12@gmail.com>
2021-04-06 13:40:14 -0600

    post added aye

--- Controllers/CarsController.cs
+++ Controllers/CarsController.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using gregslistcsharp.DB.cs;
+using gregslistcsharp.Models.cs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace gregslistcsharp.Controllers
@@ -8,6 +11,35 @@ namespace gregslistcsharp.Controllers
     [Route("api/[controller]")]
     public class CarsController : ControllerBase
     {
+        [HttpGet]
+        public ActionResult<IEnumerable<Car>> Get()
+        {
+            try
+            {
+                return Ok(FakeDB.Cars);
+            }
+            catch (System.Exception err)
+            {
+
+                return BadRequest(err.Message);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult<Car> Create([FromBody] Car newCar)
+        {
+            try
+            {
+                FakeDB.Cars.Add(newCar);
+                return Ok(newCar);
+            }
+            catch (System.Exception err)
+            {
+
+                return BadRequest(err.Message);
+            }
+        }
+
 
     }
 
